5286191bee244b4e59fd8e768c5f4dcbcdc6a8c0
Sourav Singh <sourav.singh@winfosolutions.com>
2022-10-03 15:55:04 +0530

    new-heading

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <div>hii Sourav</div>
+      <h1>added new h1 tag here</h1>
       <p>this is some dummy paragraph</p>
       <button onClick={() => setCount(count + 1)}>click {count}</button>
     </div>
