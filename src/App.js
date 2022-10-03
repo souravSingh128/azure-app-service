0d957d36c0d80b5903d97613e5a4ec4fd81d5fed
Sourav Singh <sourav.singh@winfosolutions.com>
2022-10-03 08:40:02 +0530

    button-added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <div>hii Sourav</div>
       <p>this is some dummy paragraph</p>
+      <button onClick={() => setCount(count + 1)}>click {count}</button>
     </div>
   );
 }
